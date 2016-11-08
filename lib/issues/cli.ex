5b637ff07a6e61dcc36c2e713e830ca10e8a14c1
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2016-11-09 01:54:23 +0600

    Fetch github project issues

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,7 +8,41 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+      |> parse_args
+      |> process
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+    |> convert_to_list_of_hashdicts
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, reason}) do
+    #{_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{reason}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_hashdicts(list) do
+    list
+    |> Enum.map(&Enum.into(&1, HashDict.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+              fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 
   @doc """
